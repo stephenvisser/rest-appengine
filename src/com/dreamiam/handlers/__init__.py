c4c35058d56237213d7f58da89d920f315ce014b
Stephen Visser <stephen@orderoo.com>
2012-05-01 18:18:00 -0600

    fixed erroneous link to users

--- src/com/dreamiam/handlers/__init__.py
+++ src/com/dreamiam/handlers/__init__.py
@@ -2,6 +2,5 @@ import webapp2
 
 import rest_api
 import file_manager
-import users
 
-app = webapp2.WSGIApplication([('/api.*', rest_api.Rest), ('/prepare_upload', file_manager.Prepare), ('/file_upload', file_manager.Upload), ('/file_download/([^/]+)', file_manager.Download), ('/login)', users.Login)], debug=True)
+app = webapp2.WSGIApplication([('/api.*', rest_api.Rest), ('/prepare_upload', file_manager.Prepare), ('/file_upload', file_manager.Upload), ('/file_download/([^/]+)', file_manager.Download)], debug=True)
